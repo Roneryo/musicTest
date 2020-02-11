a8648330d87f396654c184aaad1e5e7a3ad45d06
Roneryo <roner.ortega@gmail.com>
2020-02-11 17:36:17 -0500

    Cambio iminente. Agregando ventana de chat y boton para activar ventana de chat.

--- main.js
+++ main.js
@@ -1,40 +1,10 @@
-// alert("Logre funcionar!");
-async function fetchAudio(word) {
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
-    let url = 'https://api.deezer.com/track/3135556';
-    // fetch() returns a promise that
-    // resolves once headers have been received
-    return fetch(url, requestOptions)
-      .then(res => {
-        if (!res.ok)
-          throw new Error(`${res.status} = ${res.statusText}`);
-        // response.body is a readable stream.
-        // Calling getReader() gives us exclusive access to
-        // the stream's content
-        var reader = res.body.getReader();
-        // read() returns a promise that resolves
-        // when a value has been received
-        return reader
-          .read()
-          .then((result) => {
-            return result;
-          });
-      })
-  }
-  fetchAudio("word")
-    .then((response) => {
-      // response.value for fetch streams is a Uint8Array
-      var blob = new Blob([response.value], { type: 'audio/mp3' });
-      var url = window.URL.createObjectURL(blob)
-      window.audio = new Audio();
-      window.audio.src = url;
-      window.audio.play();
-    })
-    .catch((error) => {
-    console.log(error.message); 
-    });
+let chatButton = document.querySelector(".mini-chat");
+
+chatButton.addEventListener("click",function(){
+    if(chatButton.classList.contains("clicked")){
+    chatButton.classList.remove("clicked")
+    }else{
+    chatButton.classList.add("clicked");
+    }
+})
+if(chatButton.classList.contains("clicked"));
